1ce63b775a505c294e0b4a5965b5d241d41917e8
wajihshamim02 <wajihshamim2000@gmail.com>
2022-11-08 01:22:12 +0500

    updated code

--- lib/presentation/screens/complete_profile/component/complete_profile_body.dart
+++ lib/presentation/screens/complete_profile/component/complete_profile_body.dart
@@ -4,13 +4,19 @@ import 'package:sun_bright/Utilities/size_config.dart';
 import '../../sign_in/components/sign_in_form.dart';
 import 'complete_profile_form.dart';
 
-class CompleteProfileBody extends StatelessWidget {
-  late ScreenArgs userData;
-  CompleteProfileBody({Key? key, required this.userData}) : super(key: key);
+// ignore: must_be_immutable
+class CompleteProfileBody extends StatefulWidget {
+   final ScreenArgs userData;
+   const CompleteProfileBody({Key? key,required this.userData}) : super(key: key);
 
   @override
+  State<CompleteProfileBody> createState() => _CompleteProfileBodyState();
+}
+
+class _CompleteProfileBodyState extends State<CompleteProfileBody> {
+  @override
   Widget build(BuildContext context) {
-    final String userName = userData.email.split("@")[0];
+    final String userName = widget.userData.email.split("@")[0];
     return SafeArea(
       child: SizedBox(
         width: double.infinity,
@@ -27,23 +33,25 @@ class CompleteProfileBody extends StatelessWidget {
                     color: Colors.black,
                     fontSize: SizeConfig.getProportionateScreenWidth(28),
                     fontWeight: FontWeight.bold,
-                    letterSpacing: 2),
-              ),
-              const Text(
-                "Please complete your information",
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: SizeConfig.screenHeight * 0.06),
-              CompleteProfileForm(),
-              SizedBox(height: SizeConfig.getProportionateScreenHeight(30)),
-              Text(
-                "By continuing your confirm that you agree \nwith our Term and Condition",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.caption,
-              ),
-              SizedBox(height: SizeConfig.getProportionateScreenHeight(30)),
-            ],
-          )),
+                    letterSpacing: 2
+                ),
+                ),
+                const Text(
+                  "Please complete your information",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.06),
+                // CompleteProfileForm(userData: userData,),
+                SizedBox(height: SizeConfig.getProportionateScreenHeight(30)),
+                Text(
+                  "By continuing your confirm that you agree \nwith our Term and Condition",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                SizedBox(height: SizeConfig.getProportionateScreenHeight(30)),
+              ],
+            )
+          ),
         ),
       ),
     );
